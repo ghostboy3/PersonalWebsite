48aef73b2560ec6b9d5b049a7c19c99e487dbb2d
ghostboy3 <charlie.to.luu@gmail.com>
2023-08-02 11:01:58 -0400

    resume

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,6 +3,7 @@ import "./styles/intro.css";
 import About from "./About";
 import Portfolio from "./Portfolio";
 import Contact from "./Contact"
+import Resumepdf from "./assets/Resume.pdf"
 
 const Intro = () => {
   return (
@@ -18,7 +19,11 @@ const Intro = () => {
       <h1 className="teel title">Nico To</h1>
       <h3 className="gray short-bio">Welcome to my little corner on the internet!!</h3>
       <p className="gray bio">I'm a grade 10 student from William Lyon Mackenzie and I like building websites.</p>
-      {/* <button className="btn btn-outline-light">Download Resume</button> */}
+      <a href={Resumepdf}
+        download="Resume.pdf"
+        target="_blank"
+        rel="noreferrer"
+        className="btn btn-outline-light">Download Resume</a>
       {/* <button className="btn btn-outline-light">Contact Me!</button> */}
       <a href="#about" className="btn btn-outline-light">Learn About Me</a>
     </div>
